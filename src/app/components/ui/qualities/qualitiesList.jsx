deac95696a4166cc70390acd91f6c7a157d8cbfc
evgeny3322 <retard_x_98@mail.ru>
2022-10-31 20:48:20 +0300

    push dev project

---
+++ src/app/components/ui/qualities/qualitiesList.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import Quality from "./quality";
+import { useDispatch, useSelector } from "react-redux";
+import { getQualitiesByIds, getQualitiesLoadingStatus, loadQualitiesList } from "../../../store/qualities";
+
+const QualitiesList = ({ qualities }) => {
+    const dispatch = useDispatch();
+    const isLoading = useSelector(getQualitiesLoadingStatus());
+    const qualitiesList = useSelector(getQualitiesByIds(qualities));
+    useEffect(() => {
+        dispatch(loadQualitiesList());
+    }, []);
+
+    if (isLoading) return "Loading...";
+
+    return (
+        <>
+            {qualitiesList.map((qual) => (
+                <Quality key={qual} id={qual} />
+            ))}
+        </>
+    );
+};
+
+QualitiesList.propTypes = {
+    qualities: PropTypes.array
+};
+
+export default QualitiesList;
